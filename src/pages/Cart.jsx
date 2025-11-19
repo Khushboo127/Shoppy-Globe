398c3e5e08aecfe0960e0668468e8b87a0f5f4ad
Khsuhboo127 <vermakhushboo845@gmail.com>
2025-11-20 01:51:24 +0530

    Cart Page- displays shopping cart

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,7 @@
+// Cart page - displays shopping cart
+import React from 'react'
+import CartComponent from '../components/Cart/Cart'
+
+export default function CartPage() {
+    return <CartComponent />
+}
