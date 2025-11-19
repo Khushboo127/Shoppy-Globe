80e7b33efb062a89c88a7256faec3957a06b4f87
Khsuhboo127 <vermakhushboo845@gmail.com>
2025-11-20 01:58:59 +0530

    Create Layout component to wrap Header, Footer and dynamic page content using Outlet

---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+// Layout component that includes Header and Outlet
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+import Footer from './Footer'
+
+export default function Layout() {
+    return (
+        <div className="layout">
+            <Header />
+            <main className="main-content">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    )
+}
